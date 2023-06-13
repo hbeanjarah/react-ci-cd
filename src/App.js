29821e01c9b050239904d8779162958394ec77d3
hbeanjarah <hbeanjarahoussen@gmail.com>
2023-06-13 05:23:47 +0300

    feat: make test failed two

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="form-container">
-        <p>CircleCi CI/CD testidsdsdsdsddsdsdsdsdsdsd</p>
+        <p>CircleCi CI/CD testidsdsdsdsddsdsdsdssdsd</p>
         <input name="username" type="text" placeholder="username" />
         <input name="password" type="password" placeholder="password" />
         <button type="submit">Login</button>
