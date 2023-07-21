9426897a05af0fdd8264fef1b67a838e493f2cdc
hbeanjarah <hbeanjarahoussen@gmail.com>
2023-07-21 06:08:58 +0300

    [feat]: make test to fail

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="form-container">
-        <p>CircleCi CI/CD</p>
+        <p>CircleCi CI/C</p>
         <input name="username" type="text" placeholder="username" />
         <input name="password" type="password" placeholder="password" />
         <button type="submit">Login</button>
